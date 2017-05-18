f104847b7afde1a611225e87580adc5ec9fb4a56
Carlos Uy <charliieuy@gmail.com>
2017-05-18 16:00:10 +0000

    automation and transpiling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var path =  require('path');
-var open = require('open');
+/*************************************************************
+	* Cloud 9 URL
+	* http://ps-js-dev-charliieuy.c9users.io:8080
+	* Ports: 8080,8081,8082
+*************************************************************/
 
-var port = 3000;
-var app = express();
+//Common JS 
+//var express = require('express');
+//var path =  require('path');
+//var open = require('open');
+//var port = 8080;
+//var app = express();
+
+//ECS6 
+import express from 'express';
+import path from 'path';
+import open from 'open';
+const port = 8080;
+const app = express();
 
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '../src/index.html'));
