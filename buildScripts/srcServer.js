ee03aff989903e3af141e51e21a6eba6f492ae39
Carlos Uy <charliieuy@gmail.com>
2017-05-18 18:20:32 +0000

    webpack bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -15,8 +15,17 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev.js';
+
 const port = 8080;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+	noInfo: false,
+	publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '../src/index.html'));
